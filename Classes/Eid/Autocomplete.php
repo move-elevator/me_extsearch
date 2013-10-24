2f9000efcee0d2803aa007a3eb3665f759dc0b6e
Thomas Scholze <tsc@move-elevator.de>
2013-10-24 14:34:20 +0200

    #5860 added a basic autocomplete feature

---
+++ Classes/Eid/Autocomplete.php
@@ -0,0 +1,109 @@
+<?php
+namespace MoveElevator\MeExtsearch\Eid;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Utility\EidUtility;
+
+if (!(TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_FE)) {
+	die ();
+} else {
+	$object = GeneralUtility::makeInstance('MoveElevator\MeExtsearch\Eid\Autocomplete');
+	$object->main();
+}
+
+/**
+ * Class Autocomplete
+ *
+ * @package MoveElevator\MeExtsearch\Eid
+ */
+class Autocomplete {
+
+	const EXTKEY = 'meextsearch';
+
+	public function __construct() {
+		// Connect to database:
+		EidUtility::connectDB();
+	}
+
+	/**
+	 * @return void
+	 */
+	public function main() {
+		if (isset($_GET['term'])) {
+			$query = $this->createQuery($_GET['term'], $this->getLanguage());
+			$words = $this->getList($query);
+			echo json_encode($words);
+		}
+	}
+
+	/**
+	 * @param string $query
+	 * @return array
+	 */
+	protected function getList($query) {
+		$res = $GLOBALS['TYPO3_DB']->sql_query($query);
+		$results = array();
+		while ($row = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			$results[] = array(
+				'id' => $row['baseword'],
+				'label' => $row['baseword'],
+				'value' => $row['baseword'],
+			);
+		}
+
+		return $results;
+	}
+
+	/**
+	 * @param $search
+	 * @return string
+	 */
+	protected function createQuery($search) {
+		$words = GeneralUtility::trimExplode(' ', $search, 1);
+
+		$where_array = array();
+		foreach ($words as $word) {
+			$where_array[] = 'baseword LIKE ' . $GLOBALS['TYPO3_DB']->fullQuoteStr('%' . $word . '%', 'index_words');
+		}
+
+		$from = 'index_words';
+		$where = '(' . implode(' AND ', $where_array) . ')';
+
+		// Join with index_phash table
+		$from .= ',index_rel,index_phash';
+		$where .=
+			' AND index_words.wid = index_rel.wid ' .
+			' AND index_rel.phash = index_phash.phash' .
+			' AND index_phash.gr_list = "0,-1"' .
+			' AND index_phash.sys_language_uid=' . $this->getLanguage();
+
+		$query =
+			'SELECT DISTINCT baseword' .
+			' FROM ' . $from .
+			' WHERE ' . $where .
+			' ORDER BY LENGTH(baseword)' .
+			' LIMIT ' . $this->getLimit();
+
+		return $query;
+	}
+
+	protected function getLanguage() {
+		$languageId = intval($_GET['language']);
+		if ($languageId > 0) {
+			return $languageId;
+		}
+
+		return 0;
+	}
+
+	protected function getLimit() {
+		$limit = intval($_GET['limit']);
+		if ($limit > 0) {
+			return $limit;
+		}
+
+		return 7;
+	}
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -8,6 +8,8 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_indexedsearch'] = array(
 	'className' => 'MoveElevator\\MeExtsearch\\Controller\\SearchFormController',
 );
 
+$TYPO3_CONF_VARS['FE']['eID_include']['me_extsearch_autocomplete'] = 'EXT:me_extsearch/Classes/Eid/Autocomplete.php';
+
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_meextsearch_tasks_delete'] = array(
     'extension' => $_EXTKEY,
     'title' => 'Delete Search Cache',
