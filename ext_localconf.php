bc18b992f6605fc52ced8b1d1a96465a0ab3dba9
move:elevator (Jan Männig) <jma@move-elevator.de>
2015-03-20 11:07:22 +0100

    #17290 fixed settings check and remove use statement

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,11 +6,16 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-if ((bool) ExtensionSettingsUtility::checkPageBrowserOverwrite() === TRUE) {
+$meEstSearchNameSpaces = array(
+	'searchForm' => 'MoveElevator\MeExtsearch\Controller\SearchFormController',
+	'command' => 'MoveElevator\MeExtsearch\Command\IndexCommandController'
+);
+
+if ((bool) \MoveElevator\MeExtsearch\Utility\ExtensionSettingsUtility::checkPageBrowserOverwrite() === TRUE) {
 	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_indexedsearch'] = array(
-		'className' => 'MoveElevator\\MeExtsearch\\Controller\\SearchFormController',
+		'className' => $meEstSearchNameSpaces['searchForm'],
 	);
 }
 
 $TYPO3_CONF_VARS['FE']['eID_include']['me_extsearch_autocomplete'] = 'EXT:me_extsearch/Classes/Eid/Autocomplete.php';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'MoveElevator\MeExtsearch\Command\IndexCommandController';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = $meEstSearchNameSpaces['command'];
