7ddaa0142d280952b75f367da2d1db4512cef9d5
move:elevator (Jan Männig) <jma@move-elevator.de>
2015-03-20 10:19:09 +0100

    #17290 make pagebrowser overwrite deactivateable

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,16 @@
 <?php
 
+use \MoveElevator\MeExtsearch\Utility\ExtensionSettingsUtility;
+
 if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_indexedsearch'] = array(
-	'className' => 'MoveElevator\\MeExtsearch\\Controller\\SearchFormController',
-);
+if ((bool) ExtensionSettingsUtility::checkPageBrowserOverwrite() === TRUE) {
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['tx_indexedsearch'] = array(
+		'className' => 'MoveElevator\\MeExtsearch\\Controller\\SearchFormController',
+	);
+}
 
 $TYPO3_CONF_VARS['FE']['eID_include']['me_extsearch_autocomplete'] = 'EXT:me_extsearch/Classes/Eid/Autocomplete.php';
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'MoveElevator\MeExtsearch\Command\IndexCommandController';
