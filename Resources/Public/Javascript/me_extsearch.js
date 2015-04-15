423549dcb696b3ae975dcc31f9548c9886f78c03
Steve Schütze <sts@me-office365.de>
2015-04-15 15:56:45 +0200

    refactor javascript-handling in me_extsearch

---
+++ Resources/Public/Javascript/me_extsearch.js
@@ -0,0 +1,16 @@
+$( document ).ready( function() {
+	$( "#tx-indexedsearch-searchbox-sword" ).autocomplete( {
+		source: "index.php?eID=me_extsearch_autocomplete&language=" + language + "&limit=" + limit,
+		minLength: 3,
+		select: function( event, ui ) {
+			$( "#tx-indexedsearch-searchbox-sword" ).value = ui.item.value;
+			window.setTimeout( function() {
+				searchbox_autocomplete_submit()
+			}, 10 );
+		}
+	} );
+
+	var searchbox_autocomplete_submit = function() {
+		$( "#tx-indexedsearch-searchbox" ).submit();
+	};
+} );
