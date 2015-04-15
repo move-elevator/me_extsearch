363754fb5b73db74748d1857aa743b575fd192aa
move:elevator (Jan Männig) <jma@move-elevator.de>
2015-04-15 15:16:13 +0200

    Fixed naming static file

--- ext_tables.php
+++ ext_tables.php
@@ -6,5 +6,5 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
 	$_EXTKEY,
 	'Configuration/TypoScript/Example',
-	'Autocomplete example'
+	'm:e ExtSearch Autocomplete example'
 );
