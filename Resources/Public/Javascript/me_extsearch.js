8b0d1739efa2c994bfe72ef37537583320be3757
Steve Schütze <sts@me-office365.de>
2015-04-16 13:49:24 +0200

    fix typescript variable names change global javascript variables

--- Resources/Public/Javascript/me_extsearch.js
+++ Resources/Public/Javascript/me_extsearch.js
@@ -4,7 +4,7 @@ $( document ).ready( function() {
 	};
 
 	$( "#tx-indexedsearch-searchbox-sword" ).autocomplete( {
-		source: "index.php?eID=me_extsearch_autocomplete&language=" + language + "&limit=" + limit,
+		source: "index.php?eID=me_extsearch_autocomplete&language=" + meExtSearchLanguage + "&limit=" + meExtSearchLimit,
 		minLength: 3,
 		select: function( event, ui ) {
 			$( "#tx-indexedsearch-searchbox-sword" ).value = ui.item.value;
