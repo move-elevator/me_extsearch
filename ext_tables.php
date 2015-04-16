7276824ae34df1b1643c6d37aeb96e7ec9237634
move:elevator (Jan Männig) <jma@move-elevator.de>
2015-04-16 16:31:16 +0200

    Add jquerry submodule

--- ext_tables.php
+++ ext_tables.php
@@ -8,3 +8,15 @@ if (!defined('TYPO3_MODE')) {
 	'Configuration/TypoScript/Example',
 	'm:e ExtSearch Autocomplete example'
 );
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	$_EXTKEY,
+	'Configuration/TypoScript/jQuery',
+	'm:e ExtSearch Autocomplete jQuery'
+);
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+	$_EXTKEY,
+	'Configuration/TypoScript/jQueryUI',
+	'm:e ExtSearch Autocomplete jQuery UI'
+);
